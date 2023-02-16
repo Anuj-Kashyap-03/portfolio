d24ccdaa30e6ac7963958ffdf3392a8cccbfc0c9
Anuj Kashyap <anuj.kashyap.me@gmail.com>
2023-02-16 20:35:18 +0530

    update AnujKashyap.pdf and navigation animation

--- Components/Navbar/Navbar.tsx
+++ Components/Navbar/Navbar.tsx
@@ -3,22 +3,19 @@ import { motion, useCycle } from "framer-motion";
 import { MenuToggle } from "./MenuToggle";
 import styled from "@emotion/styled";
 import NavItems from "./NavItems";
-import { useDimensions } from "./Dimensions";
 
 const sidebar = {
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+  open: {
+    clipPath: `circle(1200px at 40px 40px)`,
     transition: {
       type: "spring",
-      stiffness: 20,
-      restDelta: 2,
+      stiffness: 200,
     },
-  }),
+  },
   closed: {
     clipPath: "circle(0px at 40px 40px)",
     transition: {
       type: "spring",
-      delay: 0.1,
       stiffness: 400,
       damping: 40,
     },
@@ -32,6 +29,7 @@ const NavbarDiv = styled(motion.div)({
   bottom: 0,
   zIndex: 5,
   overflowY: "scroll",
+  height: "100vh",
   width: 300,
   maxWidth: "100vw",
   backgroundColor: "var(--color-1)",
@@ -39,14 +37,10 @@ const NavbarDiv = styled(motion.div)({
 
 export const Navbar = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
-  const containerRef = React.useRef(null);
-  const { height } = useDimensions(containerRef);
 
   return (
     <motion.nav
       initial={false}
-      custom={height}
-      ref={containerRef}
       animate={isOpen ? "open" : "closed"}
     >
       <NavbarDiv variants={sidebar}>
